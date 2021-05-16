b07f74563828ae1e31ba60bfbe2f76b5fd301313
vdiqbd <82767685+vdiqbd@users.noreply.github.com>
2021-05-16 14:11:24 +0100

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -6,8 +6,13 @@ numbers = "0123456789"
 symbols = "[]{}()*;/,_-"
 
 all = lower + upper + numbers + symbols
-length = int(input("Enter Length of Password: ")) # Length should be 74 or less, because there are only 74 characters to choose from.
+while True:
+    try:
+        length = int(input("Enter Length of Password: "))
+        break
+    except ValueError:
+        print("Please select a number equal to or less than 74.")
 
 password = "".join(random.sample(all, length))
 
-print(password)
+print(f"Your password is {password}")
